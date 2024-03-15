d45c2e56d72778801b0de8f6b3e594e0b45a23e5
clementsan <cvachet@sci.utah.edu>
2024-03-15 13:13:47 -0600

    Add main python file

---
+++ main.py
@@ -0,0 +1,12 @@
+import example_package_clem.example as e_clem 
+
+def main():
+	var_addone = e_clem.add_one(2)
+	print('var_addone: ', var_addone)
+
+	var_subtractone = e_clem.subtract_one(2)
+	print('var_subtractone: ', var_subtractone)
+
+
+if __name__ == "__main__":
+	main()
